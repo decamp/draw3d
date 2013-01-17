9bbec7d9d987d0e10282c7ab536747d396e1eb5e
decamp <decamp@cogmacmacpro.local>
2013-01-16 19:04:38 -0500

    Got blending wrong again. Now using glBlendFuncSeparate( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA ).

--- src/cogmac/draw3d/nodes/BlendFuncNode.java
+++ src/cogmac/draw3d/nodes/BlendFuncNode.java
@@ -9,19 +9,40 @@ import static javax.media.opengl.GL.*;
  */
 public class BlendFuncNode extends DrawNodeAdapter {
 
+    
+    public static BlendFuncNode newSrcOver() {
+        return new BlendFuncNode( true, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA );
+    }
+    
+    
     private boolean mEnable;
-    private int mSrcFactor;
-    private int mDstFactor;
+    private int mSrcRgb;
+    private int mDstRgb;
+    private int mSrcAlpha;
+    private int mDstAlpha;
     
     private boolean mRevertEnable = true;
-    private final int[] mRevert   = { GL_ONE, GL_ZERO };
+    private final int[] mRevert   = { GL_ONE, GL_ZERO, GL_ONE, GL_ZERO };
     
+    
+    public BlendFuncNode() {} 
     
     public BlendFuncNode( boolean enable, int srcFactor, int dstFactor ) {
-        mEnable    = enable;
-        mSrcFactor = srcFactor;
-        mDstFactor = dstFactor;
+        mEnable   = enable;
+        mSrcRgb   = srcFactor;
+        mDstRgb   = dstFactor;
+        mSrcAlpha = srcFactor;
+        mDstAlpha = dstFactor;
     }
+   
+    public BlendFuncNode( boolean enable, int srcRgb, int dstRgb, int srcAlpha, int dstAlpha ) {
+        mEnable   = enable;
+        mSrcRgb   = srcRgb;
+        mDstRgb   = dstRgb;
+        mSrcAlpha = srcAlpha;
+        mDstAlpha = dstAlpha;
+    }
+    
     
     
     public boolean enable() {
@@ -33,30 +54,74 @@ public class BlendFuncNode extends DrawNodeAdapter {
         return this;
     }
     
+    
     public int srcFactor() {
-        return mSrcFactor;
+        return mSrcRgb;
     }
     
     public BlendFuncNode srcFactor( int srcFactor ) {
-        mSrcFactor = srcFactor;
+        mSrcRgb   = srcFactor;
+        mSrcAlpha = srcFactor;
         return this;
     }
     
     public int dstFactor() {
-        return mDstFactor;
+        return mDstRgb;
     }
     
     public BlendFuncNode dstFactor( int dstFactor ) {
-        mDstFactor = dstFactor;
+        mDstRgb   = dstFactor;
+        mDstAlpha = dstFactor;
         return this;
     }
+    
+    
+    public int srcRgb() {
+        return mSrcRgb;
+    }
+    
+    public BlendFuncNode srcRgb( int srcRgb ) {
+        mSrcRgb = srcRgb;
+        return this;
+    }
+    
+    public int dstRgb() {
+        return mDstRgb;
+    }
+    
+    public BlendFuncNode dstRgb( int dstRgb ) {
+        mDstRgb = dstRgb;
+        return this;
+    }
+
+    
+    public int srcAlpha() {
+        return mSrcAlpha;
+    }
+    
+    public BlendFuncNode srcAlpha( int srcAlpha ) {
+        mSrcAlpha = srcAlpha;
+        return this;
+    }
+    
+    public int dstAlpha() {
+        return mDstAlpha;
+    }
+    
+    public BlendFuncNode dstAlpha( int dstAlpha ) {
+        mDstAlpha = dstAlpha;
+        return this;
+    }
+    
     
     
     @Override
     public void pushDraw(GL gl) {
         mRevertEnable = gl.glIsEnabled( GL_BLEND );
-        gl.glGetIntegerv( GL_BLEND_SRC, mRevert, 0 );
-        gl.glGetIntegerv( GL_BLEND_DST, mRevert, 1 );
+        gl.glGetIntegerv( GL_BLEND_SRC_RGB  , mRevert, 0 );
+        gl.glGetIntegerv( GL_BLEND_DST_RGB  , mRevert, 1 );
+        gl.glGetIntegerv( GL_BLEND_SRC_ALPHA, mRevert, 2 );
+        gl.glGetIntegerv( GL_BLEND_DST_ALPHA, mRevert, 3 );
         
         if( mEnable ) {
             gl.glEnable( GL_BLEND );
@@ -64,7 +129,7 @@ public class BlendFuncNode extends DrawNodeAdapter {
             gl.glDisable( GL_BLEND );
         }
         
-        gl.glBlendFunc( mSrcFactor, mDstFactor );
+        gl.glBlendFuncSeparate( mSrcRgb, mDstRgb, mSrcAlpha, mDstAlpha );
     }
     
     @Override
@@ -75,7 +140,7 @@ public class BlendFuncNode extends DrawNodeAdapter {
             gl.glDisable( GL_BLEND );
         }
         
-        gl.glBlendFunc( mRevert[0], mRevert[1] );
+        gl.glBlendFuncSeparate( mRevert[0], mRevert[1], mRevert[2], mRevert[3] );
     }
     
 }
--- src/cogmac/draw3d/nodes/FogNode.java
+++ src/cogmac/draw3d/nodes/FogNode.java
@@ -10,61 +10,158 @@ import static javax.media.opengl.GL.*;
  * @author decamp
  */
 public class FogNode extends DrawNodeAdapter {
-    
-    
+
+
     public static FogNode newInstance() {
         return new FogNode();
     }
+
+
+    private final int[] mStateInt     = { 1, GL_EXP };
+    private final float[] mStateFloat = { 1f, 0f, 1f, 0f, 0f, 0f, 0f };
+    private final int[] mRevertInt;
+    private final float[] mRevertFloat;
     
     
-    private int[] mMode      = {GL_LINEAR};
-    private float[] mDensity = {1f};
-    private float[] mStart   = {0f};
-    private float[] mEnd     = {1f};
-    private float[] mColor   = {0,0,0,1};
-    
-    private FogNode() {}
-    
-    
-    
-    public void mode(int mode) {
-        mMode[0] = mode;
-    }
-    
-    public void density(float density) {
-        mDensity[0] = density;
-    }
-    
-    public void start(float start) {
-        mStart[0] = start;
-    }
-    
-    public void end(float end) {
-        mEnd[0] = end;
-    }
-    
-    public void color(float r, float g, float b, float a) {
-        mColor[0] = r;
-        mColor[1] = g;
-        mColor[2] = b;
-        mColor[3] = a;
+    public FogNode() {
+        this( true );
     }
     
     
+    public FogNode( boolean pushState ) {
+        if( pushState ) {
+            mRevertInt   = new int[2];
+            mRevertFloat = new float[7];
+        } else {
+            mRevertInt   = null;
+            mRevertFloat = null;
+        }
+    }
+
+
     
-    public void pushDraw(GL gl) {
-        gl.glEnable(GL_FOG);
-        gl.glFogiv(GL_FOG_MODE, mMode, 0);
-        gl.glFogfv(GL_FOG_DENSITY, mDensity, 0);
-        gl.glFogfv(GL_FOG_START, mStart, 0);
-        gl.glFogfv(GL_FOG_END, mEnd, 0);
-        gl.glFogfv(GL_FOG_COLOR, mColor, 0);
+    public boolean enable() {
+        return mStateInt[0] != 0;
+    }
+    
+    
+    public FogNode enable( boolean enable ) {
+        mStateInt[0] = enable ? 1 : 0;
+        return this;
+    }
+    
+    
+    public int mode() {
+        return mStateInt[1];
+    }
+    
+    
+    public FogNode mode( int mode ) {
+        mStateInt[1] = mode;
+        return this;
+    }
+    
+    
+    public float density() {
+        return mStateFloat[0];
+    }
+    
+    
+    public FogNode density( float density ) {
+        mStateFloat[0] = density;
+        return this;
+    }
+    
+    
+    public float start() {
+        return mStateFloat[1];
+    }
+    
+    
+    public FogNode start( float start ) {
+        mStateFloat[1] = start;
+        return this;
+    }
+    
+    
+    public float end() {
+        return mStateFloat[2];
+    }
+    
+    
+    public FogNode end( float end ) {
+        mStateFloat[2] = end;
+        return this;
+    }
+    
+    
+    public void getColor( float[] out4x1 ) {
+        out4x1[0] = mStateFloat[3];
+        out4x1[1] = mStateFloat[4];
+        out4x1[2] = mStateFloat[5];
+        out4x1[3] = mStateFloat[6];
+    }
+    
+    
+    public void setColor( float r, float g, float b, float a ) {
+        mStateFloat[3] = r;
+        mStateFloat[4] = g;
+        mStateFloat[5] = b;
+        mStateFloat[6] = a;
+    }
+    
+    
+    public void color( float r, float g, float b, float a ) {
+        mStateFloat[3] = r;
+        mStateFloat[4] = g;
+        mStateFloat[5] = b;
+        mStateFloat[6] = a;
     }
 
     
-    public void popDraw(GL gl) {
-        gl.glDisable(GL_FOG);
+    
+    public void pushDraw( GL gl ) {
+        if( mRevertInt != null ) {
+            read( gl, mRevertInt, mRevertFloat );
+        }
+        
+        write( gl, mStateInt, mStateFloat );
+    }
+    
+    
+    public void popDraw( GL gl ) {
+        if( mRevertInt != null ) {
+            write( gl, mRevertInt, mRevertFloat );
+        } else {
+            gl.glDisable( GL_FOG );
+        }
+    }
+    
+    
+    
+    
+    private static void read( GL gl, int[] ints, float[] floats ) {
+        gl.glGetIntegerv( GL_FOG,         ints,   0 );
+        gl.glGetIntegerv( GL_FOG_MODE,    ints,   1 );
+        gl.glGetFloatv(   GL_FOG_DENSITY, floats, 0 );
+        gl.glGetFloatv(   GL_FOG_START,   floats, 1 );
+        gl.glGetFloatv(   GL_FOG_END,     floats, 2 );
+        gl.glGetFloatv(   GL_FOG_COLOR,   floats, 3 );
     }
     
 
+    private static void write( GL gl, int[] ints, float[] floats ) {
+        if( ints[0] == 0 ) {
+            gl.glDisable( GL_FOG );
+        } else {
+            gl.glEnable( GL_FOG );
+        }
+        
+        gl.glFogiv( GL_FOG_MODE,    ints,   1 );
+        gl.glFogfv( GL_FOG_DENSITY, floats, 0 );
+        gl.glFogfv( GL_FOG_START,   floats, 1 );
+        gl.glFogfv( GL_FOG_END,     floats, 2 );
+        gl.glFogfv( GL_FOG_COLOR,   floats, 3 );
+    }
+    
 }
