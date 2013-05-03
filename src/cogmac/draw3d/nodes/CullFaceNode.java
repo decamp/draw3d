6c5a34b1a7e41ea8e720da208a2c664d59943bd7
decamp <decamp@cogmacmacpro.local>
2013-05-03 16:26:21 -0400

    Added a few matrix nodes to DrawNodes.

--- src/cogmac/draw3d/nodes/CullFaceNode.java
+++ src/cogmac/draw3d/nodes/CullFaceNode.java
@@ -7,70 +7,68 @@ import static javax.media.opengl.GL.*;
  * @author decamp
  */
 public class CullFaceNode extends DrawNodeAdapter {
-    
-    
-    private boolean mEnable;
-    private int mFace;
-    
-    private boolean mRevEnable;
-    private final int[] mRevFace = {GL_BACK};
-    
+
+
+    private boolean     mEnable;
+    private int         mFace;
+
+    private boolean     mRevEnable;
+    private final int[] mRevFace = { GL_BACK };
+
     
     public CullFaceNode() {
-        this(false, GL_BACK);
+        this( false, GL_BACK );
     }
-    
-    public CullFaceNode(boolean enable, int face) {
+
+    public CullFaceNode( boolean enable, int face ) {
         mEnable = enable;
-        mFace   = face;
+        mFace = face;
     }
-    
-    
-    
+
+
+
     public boolean enable() {
         return mEnable;
     }
-    
-    public CullFaceNode enable(boolean enable) {
+
+    public CullFaceNode enable( boolean enable ) {
         mEnable = enable;
         return this;
     }
-    
+
     public int face() {
         return mFace;
     }
-    
-    public CullFaceNode face(int face) {
+
+    public CullFaceNode face( int face ) {
         mFace = face;
         return this;
     }
-    
-    
+
+
     @Override
-    public void pushDraw(GL gl) {
-        mRevEnable = gl.glIsEnabled(GL_CULL_FACE);
-        gl.glGetIntegerv(GL_CULL_FACE_MODE, mRevFace, 0);
-        
-        if(mEnable) {
-            gl.glEnable(GL_CULL_FACE);
-        }else{
-            gl.glDisable(GL_CULL_FACE);
+    public void pushDraw( GL gl ) {
+        mRevEnable = gl.glIsEnabled( GL_CULL_FACE );
+        gl.glGetIntegerv( GL_CULL_FACE_MODE, mRevFace, 0 );
+
+        if( mEnable ) {
+            gl.glEnable( GL_CULL_FACE );
+        } else {
+            gl.glDisable( GL_CULL_FACE );
         }
-        
-        gl.glCullFace(mFace);
+
+        gl.glCullFace( mFace );
     }
-    
+
     @Override
-    public void popDraw(GL gl) {
-        if(mRevEnable) {
-            gl.glEnable(GL_CULL_FACE);
-        }else{
-            gl.glDisable(GL_CULL_FACE);
+    public void popDraw( GL gl ) {
+        if( mRevEnable ) {
+            gl.glEnable( GL_CULL_FACE );
+        } else {
+            gl.glDisable( GL_CULL_FACE );
         }
-        
-        gl.glCullFace(mRevFace[0]);
+
+        gl.glCullFace( mRevFace[0] );
     }
-    
-    
-    
+
 }
--- src/cogmac/draw3d/nodes/DrawNodeList.java
+++ src/cogmac/draw3d/nodes/DrawNodeList.java
@@ -12,81 +12,105 @@ import javax.media.opengl.GLAutoDrawable;
  */
 public class DrawNodeList implements DrawNode {
 
-    
-    public static DrawNodeList newDepthFirstInstance(Collection<DrawNode> actions) {
-        return new DrawNodeList(new ArrayList<DrawNode>(actions), true);
-    }
-    
-    
-    public static DrawNodeList newDepthFirstInstance(DrawNode... actions) {
-        return new DrawNodeList(Arrays.asList(actions), true);
-    }
 
-    
-    public static DrawNodeList newBreadthFirstInstance(Collection<DrawNode> actions) {
-        return new DrawNodeList(new ArrayList<DrawNode>(actions), false);
-    }
-    
-    
-    public static DrawNodeList newBreadthFirstInstance(DrawNode... actions) {
-        return new DrawNodeList(Arrays.asList(actions), false);
+    public static DrawNodeList newDepthFirst( Collection<DrawNode> actions ) {
+        return new DrawNodeList( new ArrayList<DrawNode>( actions ), true );
     }
 
 
+    public static DrawNodeList newDepthFirst( DrawNode... actions ) {
+        return new DrawNodeList( Arrays.asList( actions ), true );
+    }
+
+
+    public static DrawNodeList newBreadthFirst( Collection<DrawNode> actions ) {
+        return new DrawNodeList( new ArrayList<DrawNode>( actions ), false );
+    }
+
+
+    public static DrawNodeList newBreadthFirst( DrawNode... actions ) {
+        return new DrawNodeList( Arrays.asList( actions ), false );
+    }
     
-    
+
+
     private final List<DrawNode> mActions;
     private final boolean mDepthFirst;
+
     
-    
-    private DrawNodeList(List<DrawNode> actions, boolean depthFirst) {
+    private DrawNodeList( List<DrawNode> actions, boolean depthFirst ) {
         mActions = actions;
         mDepthFirst = depthFirst;
     }
-    
-    
-    
-    public void init(GLAutoDrawable gld) {
-        for(DrawNode a: mActions) {
-            a.init(gld);
+
+
+
+    public void init( GLAutoDrawable gld ) {
+        for( DrawNode a : mActions ) {
+            a.init( gld );
+        }
+    }
+
+
+    public void reshape( GLAutoDrawable gld, int x, int y, int w, int h ) {
+        for( DrawNode a : mActions ) {
+            a.reshape( gld, x, y, w, h );
+        }
+    }
+
+
+    public void dispose( GLAutoDrawable gld ) {
+        for( DrawNode a : mActions ) {
+            a.dispose( gld );
+        }
+    }
+
+
+    public void pushDraw( GL gl ) {
+        if( mDepthFirst ) {
+            for( DrawNode a : mActions ) {
+                a.pushDraw( gl );
+            }
+        } else {
+            for( DrawNode a : mActions ) {
+                a.pushDraw( gl );
+                a.popDraw( gl );
+            }
+        }
+    }
+
+
+    public void popDraw( GL gl ) {
+        if( mDepthFirst ) {
+            for( int i = mActions.size() - 1; i >= 0; i-- ) {
+                mActions.get( i ).popDraw( gl );
+            }
         }
     }
 
     
-    public void reshape(GLAutoDrawable gld, int x, int y, int w, int h) {
-        for(DrawNode a: mActions) {
-            a.reshape(gld, x, y, w, h);
-        }
+    
+
+    @Deprecated
+    public static DrawNodeList newDepthFirstInstance( Collection<DrawNode> actions ) {
+        return new DrawNodeList( new ArrayList<DrawNode>( actions ), true );
     }
-    
-    
-    public void dispose(GLAutoDrawable gld) {
-        for(DrawNode a: mActions) {
-            a.dispose(gld);
-        }
+
+    @Deprecated
+    public static DrawNodeList newDepthFirstInstance( DrawNode... actions ) {
+        return new DrawNodeList( Arrays.asList( actions ), true );
+    }
+
+    @Deprecated
+    public static DrawNodeList newBreadthFirstInstance( Collection<DrawNode> actions ) {
+        return new DrawNodeList( new ArrayList<DrawNode>( actions ), false );
+    }
+
+    @Deprecated
+    public static DrawNodeList newBreadthFirstInstance( DrawNode... actions ) {
+        return new DrawNodeList( Arrays.asList( actions ), false );
     }
     
 
-    public void pushDraw(GL gl) {
-        if(mDepthFirst) {
-            for(DrawNode a: mActions) {
-                a.pushDraw(gl);
-            }
-        }else{
-            for(DrawNode a: mActions) {
-                a.pushDraw(gl);
-                a.popDraw(gl);
-            }
-        }
-    }
-
-    
-    public void popDraw(GL gl) {
-        if(mDepthFirst) {
-            for(int i = mActions.size() - 1; i >= 0; i--) {
-                mActions.get(i).popDraw(gl);
-            }
-        }
-    }
     
 }
